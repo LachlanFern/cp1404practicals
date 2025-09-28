2f9c647484430772ef0c49ccb143d36bf924ebe3
LachlanFern <lachyfern@gmail.com>
2025-09-28 21:19:56 +1000

    Add score program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,32 @@
 CP1404/CP5632 - Practical
 Program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    """This is the main function that gets an input or random number and prints the result"""
+
+    #I don't know if I am supposed to keep the stuff below so I commented it out
+    #score = float(input("Enter score: "))
+    #result = score_result(score)
+    #print(result)
+
+    score = random.randint(0,100)
+    result = score_result(score)
+    print(result)
+
+
+def score_result(score: float):
+    """This function sorts the score into a result"""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
