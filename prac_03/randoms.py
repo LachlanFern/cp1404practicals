fef74cc641613df0cb01ca20af2a04a0b8ea8c75
LachlanFern <lachyfern@gmail.com>
2025-10-05 11:45:08 +1000

    answer questions and add program to produce random number

---
+++ prac_03/randoms.py
@@ -0,0 +1,7 @@
+#On line 1 the smallest number that could be seen is 5 and the largest is 20
+#On line 2 the smallest number that could be seen is 3 and the largest is 9 and 4 is not possible because it steps by 2 starting at 3
+#On line 3 the smallest number that could be seen is around 2.5 and 5.5 as I got 2.5114475713563063 which could be rounded to 2.5, and I assume same for 5.5
+
+import random
+
+print(random.randint(1,100))
