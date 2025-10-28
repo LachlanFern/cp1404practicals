050a5d17f1cc03c0588b296ac3e4ae8754572650
jd115447 <lachyfern@gmail.com>
2025-10-28 20:01:42 +1000

    Complete program to get user input and display guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,18 +1,20 @@
 """
 guitars
 Estimate: 60 mins
-Actual:
+Actual:31 mins
 """
 from prac_06.guitar import Guitar
-
-guitars = [Guitar("Gibson L-5 CES", 1922, 16035.40),
-           Guitar("Line 6 JTV-59", 2010, 1512.9)]
+guitars = []
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitars.append(Guitar(name, year, cost))
+    print(f"{name} ({year}) : ${cost} added.")
+    name = input("Name: ")
 
 for i, guitar in enumerate(guitars, 1):
     vintage_string = "(vintage)" if guitar.is_vintage() == True else ""
-
-
-
     print(f"Guitar {i}: {guitar.name:>15} ({guitar.year}), worth $ {guitar.cost:10,.2f} {vintage_string}")
 
 
