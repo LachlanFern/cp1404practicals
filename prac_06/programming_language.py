042ba89e2b8b75fe33b60ab894ff7edeaaec20ef
jd115447 <lachyfern@gmail.com>
2025-10-28 11:26:11 +1000

    add class and set parameters passed in

---
+++ prac_06/programming_language.py
@@ -0,0 +1,22 @@
+"""
+Programming Languages
+Estimate: 30 mins
+Actual:
+"""
+
+
+
+class ProgrammingLanguage:
+    def __init__(self,name = "", typing = "", reflection = "", year = 0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self .year}"
+
+    def is_dynamic(self):
+        return self.typing == "dynamic"
+
+
