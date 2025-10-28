393523979e7fd2b71585ebc28462cf35ab04dff4
jd115447 <lachyfern@gmail.com>
2025-10-29 09:15:33 +1000

    Add docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,22 +1,25 @@
 """
 Programming Languages
 Estimate: 30 mins
-Actual:
+Actual:35 mins
 """
 
 
 
 class ProgrammingLanguage:
     def __init__(self,name = "", typing = "", reflection = "", year = 0):
+        """Initialise programming language instance"""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def __repr__(self):
-        return f"{self.name}\n"
+        """returns string of name of programming language"""
+        return f"{self.name}"
 
     def is_dynamic(self):
+        """Returns True if programming language 'typing' is Dynamic"""
         return self.typing == "Dynamic"
 
 
