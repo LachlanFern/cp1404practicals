839cb5e950619a75ec533e6f0a7e83642be44441
LachlanFern <lachlan.fern@my.jcu.edu.au>
2025-11-23 16:34:29 +1000

    Adjust string/print

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -8,6 +8,7 @@ from prac_09.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
     price_per_km = 1.23
+
     def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
@@ -15,7 +16,8 @@ class Taxi(Car):
 
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
-        return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
+        return (f"{super().__str__()}, {self.current_fare_distance}km on current fare, current fare cost:"
+                f" ${self.get_fare()}, ${self.price_per_km:.2f}/km")
 
     def get_fare(self):
         """Return the price for the taxi trip."""
--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,10 +3,10 @@ from prac_09.taxi import Taxi
 
 my_taxi = Taxi("Prius 1", 100)
 my_taxi.drive(40)
-print(f"The taxi is a {my_taxi.name} with {my_taxi.fuel} units of fuel and the fare is ${my_taxi.get_fare()}")
+print(my_taxi)
 my_taxi.start_fare()
 my_taxi.add_fuel(40)
 my_taxi.drive(100)
-print(f"The taxi is a {my_taxi.name} with {my_taxi.fuel} units of fuel and the fare is ${my_taxi.get_fare()}")
+print(my_taxi)
 
 
